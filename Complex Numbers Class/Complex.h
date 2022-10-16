3962665b10f39ef8840d337b338428645db1b03a
Yeshal Khan <yeshalkhan13@gmail.com>
2022-10-17 00:19:02 +0500

    Update Complex.h

--- Complex Numbers Class/Complex.h
+++ Complex Numbers Class/Complex.h
@@ -8,13 +8,13 @@ class Complex
 public:
 	Complex(float a = 0, float b = 0);  //Constructor
 	Complex(Complex& c);  //Copy Constructor
-	friend Complex& operator+(Complex& c1, Complex& c2);  //Addition operator
-	friend Complex& operator-(Complex& c1, Complex& c2);  //Subtraction operator
-	friend Complex& operator*(Complex& c1, Complex& c2);  //Multiplication operator
-	friend Complex& operator/(Complex& c1, Complex& c2);  //Division operator
-	friend ostream& operator<<(ostream& outObj, Complex& c);  //Stream Extraction operator
-	friend istream& operator>>(istream& inObj, Complex& c);   //Stream Insertion operator
-	Complex& operator=(Complex& c); //Assignment operator
+	friend Complex& operator+(Complex& c1, Complex& c2);  // overload binary + operator
+	friend Complex& operator-(Complex& c1, Complex& c2);  // overload binary - operator
+	friend Complex& operator*(Complex& c1, Complex& c2);  // overload binary * operator
+	friend Complex& operator/(Complex& c1, Complex& c2);  // overload binary / operator
+	friend ostream& operator<<(ostream& outObj, Complex& c);  // overload stream extraction operator
+	friend istream& operator>>(istream& inObj, Complex& c);   // overload stream insertion operator
+	Complex& operator=(Complex& c); // overload assignment operator
 	float getReal() { return real; }  //Accessor for Real part
 	float getImag() { return imag; }  //Accessor for Imaginary part
 };
