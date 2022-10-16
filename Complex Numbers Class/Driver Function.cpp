a21c61d2bf85ca663419217bb41f4c58907183d9
Yeshal Khan <yeshalkhan13@gmail.com>
2022-10-17 00:20:24 +0500

    Update Driver Function.cpp

--- Complex Numbers Class/Driver Function.cpp
+++ Complex Numbers Class/Driver Function.cpp
@@ -28,11 +28,11 @@ Complex& operator/(Complex& c1, Complex& c2)
 {
 	Complex c3;
 	c3.real = c2.real;
-	c3.imag = (-1) * c2.imag;        //minus because if c2=a+bi c1 has to be multiplied with (a - bi)
+	c3.imag = (-1) * c2.imag;        //minus because if c2=a+bi, c1 has to be multiplied with (a - bi)
 	Complex N;
 	float D;
 	N = c1 * c3;
-	D = c2.real * c2.real + pow(c2.imag, 2);      //plus because minus of formula(a^2 +b^2) & minus of iota^2 = +1
+	D = c2.real * c2.real + pow(c2.imag, 2);      //plus because (minus of formula(a^2 + b^2) & minus of iota^2) = +1
 	c3.real = (N.real / D);
 	c3.imag = (N.imag / D);
 	return c3;
